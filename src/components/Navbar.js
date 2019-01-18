02d5b8ea9045cd73de66a0f547c40129e4e41043
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2019-01-17 19:56:31 -0600

    Fix Gatsby config, add a draft of a navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
 
@@ -35,7 +35,8 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          TomAlcala
+          {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -49,23 +50,18 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
         <Link className="navbar-item" to="/contact">
           Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
         </Link>
       </div>
       <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/ThomasMarcel"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <span>Curious About the Source code?</span>&nbsp;&nbsp;
           <span className="icon">
             <img src={github} alt="Github" />
           </span>
