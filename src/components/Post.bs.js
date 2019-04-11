f843c82e12b6d2338b15332c7934017e670dd5a8
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2019-04-11 15:14:32 -0500

    feat: update post, simple and social components to react hooks syntax

--- src/components/Post.bs.js
+++ src/components/Post.bs.js
@@ -9,66 +9,48 @@ function str(prim) {
   return prim;
 }
 
-var component = ReasonReact.statelessComponent("Post");
-
-function make(post, index, _children) {
-  return /* record */[
-          /* debugName */component[/* debugName */0],
-          /* reactClassInternal */component[/* reactClassInternal */1],
-          /* handedOffState */component[/* handedOffState */2],
-          /* willReceiveProps */component[/* willReceiveProps */3],
-          /* didMount */component[/* didMount */4],
-          /* didUpdate */component[/* didUpdate */5],
-          /* willUnmount */component[/* willUnmount */6],
-          /* willUpdate */component[/* willUpdate */7],
-          /* shouldUpdate */component[/* shouldUpdate */8],
-          /* render */(function (_self) {
-              return React.createElement("div", undefined, React.createElement("div", {
-                              className: "bg-image bg" + String(index)
-                            }, React.createElement("div", {
-                                  className: "layer opacity-dark"
-                                })), React.createElement("div", {
-                              key: post.id,
-                              className: "content",
-                              style: {
-                                borderBottom: "1px solid #333",
-                                padding: "2em 4em",
-                                position: "relative"
-                              }
-                            }, React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Gatsby$ReactTemplate.Link[/* make */0](post.fields.slug, "has-text-light bold", undefined, /* array */[post.frontmatter.title]))), React.createElement("p", undefined, post.excerpt), React.createElement("div", {
-                                  className: "columns"
-                                }, React.createElement("div", {
-                                      className: "column"
-                                    }, ReasonReact.element(undefined, undefined, Gatsby$ReactTemplate.Link[/* make */0](post.fields.slug, "btn parallelogram has-background-link", undefined, /* array */[React.createElement("div", {
-                                                    className: "skew-fix"
-                                                  }, "Read More", React.createElement("i", {
-                                                        className: "fas fa-caret-right",
-                                                        style: {
-                                                          marginLeft: "10px"
-                                                        }
-                                                      }))]))), React.createElement("div", {
-                                      className: "column"
-                                    }, React.createElement("div", {
-                                          className: "is-pulled-right"
-                                        }, React.createElement("i", {
-                                              className: "far fa-clock"
-                                            }), React.createElement("small", undefined, "  " + post.frontmatter.date))))));
-            }),
-          /* initialState */component[/* initialState */10],
-          /* retainedProps */component[/* retainedProps */11],
-          /* reducer */component[/* reducer */12],
-          /* jsElementWrapped */component[/* jsElementWrapped */13]
-        ];
+function Post(Props) {
+  var post = Props.post;
+  var index = Props.index;
+  return React.createElement("div", undefined, React.createElement("div", {
+                  className: "bg-image bg" + String(index)
+                }, React.createElement("div", {
+                      className: "layer opacity-dark"
+                    })), React.createElement("div", {
+                  key: post.id,
+                  className: "content",
+                  style: {
+                    borderBottom: "1px solid #333",
+                    padding: "2em 4em",
+                    position: "relative"
+                  }
+                }, React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Gatsby$ReactTemplate.Link[/* make */0](post.fields.slug, "has-text-light bold", undefined, /* array */[post.frontmatter.title]))), React.createElement("p", undefined, post.excerpt), React.createElement("div", {
+                      className: "columns"
+                    }, React.createElement("div", {
+                          className: "column"
+                        }, ReasonReact.element(undefined, undefined, Gatsby$ReactTemplate.Link[/* make */0](post.fields.slug, "btn parallelogram has-background-link", undefined, /* array */[React.createElement("div", {
+                                        className: "skew-fix"
+                                      }, "Read More", React.createElement("i", {
+                                            className: "fas fa-caret-right",
+                                            style: {
+                                              marginLeft: "10px"
+                                            }
+                                          }))]))), React.createElement("div", {
+                          className: "column"
+                        }, React.createElement("div", {
+                              className: "is-pulled-right"
+                            }, React.createElement("i", {
+                                  className: "far fa-clock"
+                                }), React.createElement("small", undefined, "  " + post.frontmatter.date))))));
 }
 
-var $$default = ReasonReact.wrapReasonForJs(component, (function (jsProps) {
-        return make(jsProps.post, jsProps.index, /* array */[]);
-      }));
+var make = Post;
+
+var $$default = Post;
 
 exports.str = str;
-exports.component = component;
 exports.make = make;
 exports.$$default = $$default;
 exports.default = $$default;
 exports.__esModule = true;
-/* component Not a pure module */
+/* react Not a pure module */
--- src/components/Post.re
+++ src/components/Post.re
@@ -22,66 +22,48 @@ type post = {
   id: string
 };
 
-/* This is the basic component. */
-let component = ReasonReact.statelessComponent("Post");
-
-[@bs.deriving abstract]
-type jsProps = {
-  post: post,
-  index: int,
-};
-
-let make = (~post, ~index, _children) => {
-  ...component,
-  render: _self => {
-    <div>
-      <div className=("bg-image bg" ++ string_of_int(index))>
-        <div className="layer opacity-dark" />
-      </div>
-      <div
-        className="content"
-        style=(ReactDOMRe.Style.make(~borderBottom="1px solid #333",
-                                     ~padding="2em 4em",
-                                     ~position="relative", ()))
-        key=(post->idGet)
-      >
-        <p>
-          <Link className="has-text-light bold"
-          href=(post->fieldsGet->slugGet) >
-            (str(post->frontmatterGet->titleGet))
-          </Link>
-        </p>
-        <p>
-          (str(post->excerptGet))
-        </p>
-        <div className="columns">
-          <div className="column">
-            <Link className="btn parallelogram has-background-link"
-              href=(post->fieldsGet->slugGet) >
-              <div className="skew-fix">
-                (str("Read More"))
-                <i className="fas fa-caret-right"
-                style=(ReactDOMRe.Style.make(~marginLeft="10px", ())) ></i>
-              </div>
-            </Link>
-          </div>
-          <div className="column">
-            <div className="is-pulled-right">
-              <i className="far fa-clock" />
-              <small>(str("  " ++ post->frontmatterGet->dateGet))</small>
+[@react.component]
+let make = (~post: post, ~index: int) => {
+  <div>
+    <div className=("bg-image bg" ++ string_of_int(index))>
+      <div className="layer opacity-dark" />
+    </div>
+    <div
+      className="content"
+      style=(ReactDOMRe.Style.make(~borderBottom="1px solid #333",
+                                   ~padding="2em 4em",
+                                   ~position="relative", ()))
+      key=(post->idGet)
+    >
+      <p>
+        <Link className="has-text-light bold"
+        href=(post->fieldsGet->slugGet) >
+          (str(post->frontmatterGet->titleGet))
+        </Link>
+      </p>
+      <p>
+        (str(post->excerptGet))
+      </p>
+      <div className="columns">
+        <div className="column">
+          <Link className="btn parallelogram has-background-link"
+            href=(post->fieldsGet->slugGet) >
+            <div className="skew-fix">
+              (str("Read More"))
+              <i className="fas fa-caret-right"
+              style=(ReactDOMRe.Style.make(~marginLeft="10px", ())) ></i>
             </div>
+          </Link>
+        </div>
+        <div className="column">
+          <div className="is-pulled-right">
+            <i className="far fa-clock" />
+            <small>(str("  " ++ post->frontmatterGet->dateGet))</small>
           </div>
         </div>
       </div>
     </div>
-  }
+  </div>
 };
 
-let default =
-  ReasonReact.wrapReasonForJs(~component, jsProps =>
-    make(
-      ~post=jsProps->postGet,
-      ~index=jsProps->indexGet,
-      [||],
-    )
-  );
+let default = make;
